85faf1b007acea2d1b4863a7c357117d8f5c5cc1
Joshua Soto <joshuasotomayor76@gmail.com>
2025-10-27 10:35:34 -0600

    Prueba web sin typescrit

--- flet_app.py
+++ flet_app.py
@@ -655,4 +655,4 @@ def main(page: ft.Page):
 # RUN APPLICATION
 # ============================================
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, view=ft.WEB_BROWSER)
